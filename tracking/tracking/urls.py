17e137d62f1b069caa34948ac157b7d1ab6f8a9e
Igor <kozlov1son@gmail.com>
2026-02-07 18:29:37 +0200

    В

--- tracking/tracking/urls.py
+++ tracking/tracking/urls.py
@@ -1,22 +1,19 @@
-"""
-URL configuration for tracking project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
 from django.urls import path
+from tasks import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("", views.TaskListView.as_view(), name="task-list"),
+    path("<int:pk>/", views.TaskDetailView.as_view(), name="task-detail"),
+    path("<int:pk>/update/", views.TaskUpdateView.as_view(), name="task-update"),
+    path("<int:pk>/delete/", views.TaskDeleteView.as_view(), name="task-delete"),
+    path("task-create/", views.TaskCreateView.as_view(), name="task-create"),
+    path("<int:pk>/complete/", views.TaskCompleteView.as_view(), name="task-complete"),
+    path('comment/edit/<int:pk>/', views.CommentUpdateView.as_view(), name='edit_comment'),
+    path('comment/delete/<int:pk>/', views.CommentDeleteView.as_view(), name='delete_comment'),
+    path('comment/like/<int:pk>/', views.CommentLikeToggle.as_view(), name='comment-like-toggle'),
+    path("login/", views.CustomLoginView.as_view(), name="login"),
+    path("logout/", views.CustomLogoutView.as_view(), name="logout"),
+    path("register/", views.RegisterView.as_view(), name="register"),
 ]
+
+app_name = "tasks"
